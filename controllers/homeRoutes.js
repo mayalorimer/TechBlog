d97f90951ccb7b7bc25d7a84724144a3758ad87c
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-10 15:21:53 -0700

    bug fixing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,5 +5,14 @@ router.get('/', async (req, res) => {
 });
 
 
+// render the login handlebar page
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const sess = {
   store: new SequelizeStore({
     db: sequelize
   })
-};
+}; 
 
 app.use(session(sess));
 
